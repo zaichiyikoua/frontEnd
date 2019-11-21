add3109aa400ff41c476dfdff799e0324933df7d
zaichiyikou <913475430@qq.com>
2019-11-21 12:25:22 +0800

    初始化项目

---
+++ front/src/main.js
@@ -0,0 +1,30 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+//路由
+import router from './router'
+//状态管理
+import store from './store'
+//axios 请求
+import axios from 'axios'
+
+//导入UI框架
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
+Vue.prototype.$axios = axios
+
+Vue.use(BootstrapVue)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
